9ed5b05034d4e80a90e5d93343e6429186937777
solom0001 <somw0001@stud.kea.dk>
2025-08-26 14:12:16 +0200

    gæt et tal excercise

---
+++ gæt_et_tal/script.js
@@ -0,0 +1,37 @@
+const bigNumb = document.getElementById("bigNumber");
+const guess = document.getElementById("userGuess");
+const checkBtn = document.getElementById("checkBtn");
+const feedback = document.getElementById("feedback");
+
+const randomNumb = () => console.log("tilfældigt tal " + Math.floor(Math.random() * 101));
+
+let rdNumb = Math.floor(Math.random() * 100) + 1;
+// function updatedNumb() {
+//   let numb = "tilfældigt tal " + Math.floor(Math.random() * 101);
+//   bigNumb.textContent = numb;
+// }
+
+// const updatedNumb = () => (bigNumb.textContent = `tilfældigt tal = ${Math.floor(Math.random() * 101)}`);
+
+// updatedNumb();
+
+checkBtn.addEventListener("click", checkGuess);
+
+function checkGuess() {
+  //   guess.addEventListener("input", () => {
+  //     if (guess != null && guess == Number) {
+  //       checkGuess;
+  //     } else {
+  //       return console.error("The guess wasn't valid");
+  //     }
+  //   });
+  const userGuess = parseInt(guess.value);
+
+  if (userGuess === rdNumb) {
+    feedback.textContent = `DU FIK PRÆCIS DEN RIGTIG som er ${rdNumb}`;
+  } else if (userGuess < rdNumb) {
+    feedback.textContent = `DESVÆRE VAR NUMMERET IKKE RIGTIGT, DEN VAR STØRRE END ${userGuess}`;
+  } else if (userGuess > rdNumb) {
+    feedback.textContent = `DESVÆRE VAR NUMMERET IKKE RIGTIGT, DEN VAR MINDRE END ${userGuess}`;
+  }
+}
